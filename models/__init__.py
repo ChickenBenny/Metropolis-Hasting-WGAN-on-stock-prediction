85f4930846153d8375401b53cdcd5cdfb78f745c
ChickenBenny <zxc123benny14159@gmail.com>
2022-12-11 14:59:09 +0800

    fix: bug in vae model

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .vae import VAE
+from .wgan import WGAN
+from .mhgan import MHGAN
--- models/vae.py
+++ models/vae.py
@@ -5,11 +5,12 @@ import numpy as np
 from .encoder import Encoder
 from .decoder import Decoder
 
-class VAE_Network(nn.Module):
-    def __init__(self, config, latent_dim):
+class VAE(nn.Module):
+    def __init__(self, config, latent_dim, use_cuda = 1):
         super().__init__()
         self.encoder = Encoder(config, latent_dim)
         self.decoder = Decoder(config, latent_dim)
+        self.device = torch.device("cuda" if (torch.cuda.is_available() & use_cuda) else "cpu")
 
     def reparameterize(self, mu, logVar):
         std = torch.exp(0.5* logVar)
@@ -22,37 +23,8 @@ class VAE_Network(nn.Module):
         output = self.decoder(z)
         return output, z, mu, logVar        
 
-class VAE(nn.Module):
-    def __init__(self, config, latent_dim, use_cuda):
-        super().__init__()
-        self.vae = VAE_Network(config, latent_dim)
-        self.device = torch.device("cuda" if (torch.cuda.is_available() & use_cuda) else "cpu")
-
     def kl_divergence(self, mu, logVar):
         return 0.5* torch.sum(-1 -logVar + mu.pow(2) + logVar.exp())
 
-    def vae_loss(self, x_hat, x, mu, logVar):
+    def criterion(self, x_hat, x, mu, logVar):
         return F.binary_cross_entropy(x_hat, x) + self.kl_divergence(mu, logVar)
-
-    def optimizers(self, lr):
-        self.opt = torch.optim.Adam(self.vae.parameters(), lr = lr)
-
-    def training_step(self, epochs, train_dataloader):
-        self.vae.train()
-        hist = np.zeros(epochs)
-        for epoch in range(epochs):
-            loss_VAE = []
-            for (x,) in train_dataloader:
-                x = x.to(self.device)
-                x_hat, z, mu, logVar = self.vae(x)
-                loss = self.vae_loss(x_hat, x, mu, logVar)
-                loss.backward()
-                self.opt.step()
-                loss_VAE.append(loss.item())
-            hist[epoch] = sum(loss_VAE)
-            print(f'[{epoch + 1}/{epochs}] LossD: {sum(loss_VAE):.5f}')
-
-    def predict(self, x):
-        self.vae.eval()
-        x_hat, z, mu, logVar = self.vae(x.to(self.device))
-        return x_hat.cpu().detach().numpy(), z.cpu().detach().numpy()
