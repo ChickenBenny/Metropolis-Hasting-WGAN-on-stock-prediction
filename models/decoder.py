0820bc3b87ad0831e23fb51cf1730a3ef226c7e2
ChickenBenny <zxc123benny14159@gmail.com>
2023-06-29 22:57:47 +0800

    Complete the VAE part of demo and rewrite VAE component

--- models/decoder.py
+++ models/decoder.py
@@ -6,21 +6,24 @@ class Decoder(nn.Module):
         super().__init__()
 
         modules = []
-        prev_dim = latent_dim
+        self.decoder_input = nn.Linear(latent_dim, config[-1])
 
-        for dim in config:
+        for i in range(len(config) - 1, 1, -1):
             modules.append(
                 nn.Sequential(
-                    nn.Linear(prev_dim, dim),
+                    nn.Linear(config[i], config[i - 1]),
                     nn.ReLU()
                 )
             )
-            prev_dim = dim
-
-        self.decoder_input = nn.Linear(latent_dim, config[-1])
+        modules.append(
+            nn.Sequential(
+                nn.Linear(config[1], config[0]),
+                nn.Sigmoid()
+            )
+        )
         self.decoder = nn.Sequential(*modules)
 
     def forward(self, x):
-        x = self.decoder_input(x)
         decoded = self.decoder(x)
         return decoded
+    
--- models/vae.py
+++ models/vae.py
@@ -6,25 +6,26 @@ from .encoder import Encoder
 from .decoder import Decoder
 
 class VAE(nn.Module):
-    def __init__(self, config, latent_dim, use_cuda = 1):
+    def __init__(self, config, latent_dim, use_cuda=1):
         super().__init__()
         self.encoder = Encoder(config, latent_dim)
         self.decoder = Decoder(config, latent_dim)
-        self.device = torch.device("cuda" if (torch.cuda.is_available() & use_cuda) else "cpu")
+        self.device = torch.device("cuda" if (torch.cuda.is_available() and use_cuda) else "cpu")
 
-    def reparameterize(self, mu, logVar):
-        std = torch.exp(0.5* logVar)
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
         eps = torch.randn_like(std)
         return eps * std + mu
 
     def forward(self, x):
-        mu, logVar = self.encoder(x)
-        z = self.reparameterize(mu, logVar)
+        mu, log_var = self.encoder(x)
+        z = self.reparameterize(mu, log_var)
         output = self.decoder(z)
-        return output, z, mu, logVar        
+        return output, z, mu, log_var
 
-    def kl_divergence(self, mu, logVar):
-        return 0.5* torch.sum(-1 -logVar + mu.pow(2) + logVar.exp())
+    def kl_divergence(self, mu, log_var):
+        return 0.5 * torch.sum(-1 - log_var + mu.pow(2) + log_var.exp())
 
-    def criterion(self, x_hat, x, mu, logVar):
-        return F.binary_cross_entropy(x_hat, x) + self.kl_divergence(mu, logVar)
+    def criterion(self, x_hat, x, mu, log_var):
+        return F.binary_cross_entropy(x_hat, x) + self.kl_divergence(mu, log_var)
+    
